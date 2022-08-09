d4704f7eb1438142475f25357018c0b90a0d1715
Baptiste Leproux <leproux.baptiste@gmail.com>
2022-08-09 11:48:34 +0200

    lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  github: {
+    disableCache: true,
+    maxContributors: 10,
+    token: process.env.GITHUB_TOKEN,
+    api: 'https://api.github.com'
+  }
 })
